d3639cc397216d4572d858dd749dc116400bbd76
Julesntizimira <ntizimijules5@gmail.com>
2023-08-30 11:55:14 +0000

    task 25

---
+++ 111-bst_insert.c
@@ -0,0 +1,57 @@
+# include "binary_trees.h"
+/**
+ * bst_insert - a function that inserts a
+ * value in a Binary Search Tree
+ *
+ * @tree: a double pointer to the root node of
+ * the BST to insert the value
+ *
+ * @value: the value to store in the node to be inserted
+ *
+ * Return: a pointer to the created node, or NULL on failure
+ * If the address stored in tree is NULL, the created node
+ * must become the root node.
+ * If the value is already present in the tree, it must be ignored
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	bst_t *newnode = NULL, *tmp = NULL;
+
+	newnode = malloc(sizeof(bst_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->parent = newnode->left = newnode->right = NULL;
+	newnode->n = value;
+	if (*tree == NULL)
+	{
+		*tree = newnode;
+		return (newnode);
+	}
+	tmp = *tree;
+	while (tmp != NULL)
+	{
+		if (value < tmp->n)
+		{
+			if (tmp->left == NULL)
+			{
+				newnode->parent = tmp;
+				tmp->left = newnode;
+				break;
+			}
+			tmp = tmp->left;
+		}
+		else if (value > tmp->n)
+		{
+			if (tmp->right == NULL)
+			{
+				newnode->parent = tmp;
+				tmp->right = newnode;
+				break;
+			}
+			tmp = tmp->right;
+		}
+		else if (value == tmp->n)
+			return (NULL);
+	}
+	return (newnode);
+}
