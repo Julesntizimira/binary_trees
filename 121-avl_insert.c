1d4e16b9132e330c936dfdd64205317328862cd2
Julesntizimira <ntizimijules5@gmail.com>
2023-08-31 12:49:13 +0000

    insert avl tree

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -16,47 +16,47 @@
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-        bst_t *newnode = NULL, *tmp = *tree;
+	bst_t *newnode = NULL, *tmp = *tree;
 
-        newnode = malloc(sizeof(bst_t));
-        if (newnode == NULL)
-             return (NULL);
-        newnode->parent = newnode->left = newnode->right = NULL;
-        newnode->n = value;
-        if (*tree == NULL)
-             *tree = newnode;
-        while (tmp != NULL)
-        {
-             if (value < tmp->n)
-             {
-                if (tmp->left == NULL)
-                {
-                        newnode->parent = tmp;
-                        tmp->left = newnode;
-                        break;
-                }
-                tmp = tmp->left;
-             }
-             else if (value > tmp->n)
-             {
-                if (tmp->right == NULL)
-                {
-                        newnode->parent = tmp;
-                        tmp->right = newnode;
-                        break;
-                }
-                tmp = tmp->right;
-             }
-             else if (value == tmp->n)
-             {
-                free(newnode);
-                return (NULL);
-             }
-        }
-        return (newnode);
+	newnode = malloc(sizeof(bst_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->parent = newnode->left = newnode->right = NULL;
+	newnode->n = value;
+	if (*tree == NULL)
+		*tree = newnode;
+	while (tmp != NULL)
+	{
+		if (value < tmp->n)
+		{
+			if (tmp->left == NULL)
+			{
+				newnode->parent = tmp;
+				tmp->left = newnode;
+				break;
+			}
+			tmp = tmp->left;
+		}
+		else if (value > tmp->n)
+		{
+			if (tmp->right == NULL)
+			{
+				newnode->parent = tmp;
+				tmp->right = newnode;
+				break;
+			}
+			tmp = tmp->right;
+		}
+		else if (value == tmp->n)
+		{
+			free(newnode);
+			return (NULL);
+		}
+	}
+	return (newnode);
 }
 /**
- * binary_tree_balance_root - a function that measures
+ * tree_balancer - a function that measures
  * the balance factor of a binary tree from bottom-top
  * post order traversal
  * check if the whole tree is balanced
@@ -64,23 +64,22 @@ bst_t *bst_insert(bst_t **tree, int value)
  * @tree: a pointer to the root node of the
  * tree to measure the balance factor
  *
- * Return: the balance factor of a binary tree
- * If tree is NULL, return 0
+ * Return: the unbalanced node or NULL
  */
 bst_t *tree_balancer(binary_tree_t *tree)
 {
-        int b_factor;
+	int b_factor;
 	binary_tree_t *tmp = NULL;
 
-        if (tree == NULL)
-                return NULL;
-        if (tree->left != NULL)
+	if (tree == NULL)
+		return (NULL);
+	if (tree->left != NULL)
 	{
 		tmp = tree_balancer(tree->left);
 		if (tmp != NULL)
 			return (tmp);
 	}
-        if (tree->right != NULL)
+	if (tree->right != NULL)
 	{
 		tmp = tree_balancer(tree->right);
 		if (tmp != NULL)
@@ -93,68 +92,47 @@ bst_t *tree_balancer(binary_tree_t *tree)
 		tmp = tree;
 		return (tmp);
 	}
-	return NULL;	
+	return (NULL);
 }
 /**
  * avl_insert - a function that inserts a value in an AVL Tree
- * 
+ *
  * @tree: a double pointer to the root node of the
  * AVL tree for inserting the value
- * 
+ *
  * @value: the value to store in the node to be inserted
- * 
+ *
  * Return: a pointer to the created node, or NULL on failure
- * 
+ *
  * If the address stored in tree is NULL,
- * 
+ *
  * the created node must become the root node.
  * The resulting tree after insertion, must be a balanced AVL Tree
  */
-
-/*binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-int binary_tree_balance(const binary_tree_t *tree);
-
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);*/
-
-
 avl_t *avl_insert(avl_t **tree, int value)
 {
 	int b_factor;
 	avl_t *tmp = NULL, *node = NULL;
 
 	tmp = bst_insert(tree, value);
-	
 	if (tmp == NULL)
 		return (NULL);
-	
 	node = tree_balancer(*tree);
-
-
-
 	if (node != NULL)
 	{
 		b_factor = binary_tree_balance(node);
-	       	
 		if (b_factor < -1)
-                {
-                        if (binary_tree_balance(node->right) > 0)
-                        {
-                                binary_tree_rotate_right(node->right);
-                        }
-                        binary_tree_rotate_left(node);
-                }
-                else
-                {
-                        if (binary_tree_balance(node->left) < 0)
-                        {
-                                binary_tree_rotate_left(node->left);
-                        }
-                        binary_tree_rotate_right(node);
-                }
-
+		{
+			if (binary_tree_balance(node->right) > 0)
+				binary_tree_rotate_right(node->right);
+			binary_tree_rotate_left(node);
+		}
+		else
+		{
+			if (binary_tree_balance(node->left) < 0)
+				binary_tree_rotate_left(node->left);
+			binary_tree_rotate_right(node);
+		}
 	}
-
-
 	return (tmp);
 }
