13d3dcc442434133378e47bf47a8df4ad13e25af
Julesntizimira <ntizimijules5@gmail.com>
2023-09-01 00:05:32 +0000

    is heap

--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,57 +1,86 @@
 # include "binary_trees.h"
-# include <stdio.h>
-int *is_heap_helper(const binary_tree_t *tree)
+/**
+ * sides_check - check node children both sides
+ * @tree: the node to check
+ * Return: int
+ */
+int sides_check(const binary_tree_t *tree)
 {
-        static size_t count = 1, i = 1, idx = 1, flag = 1;
-	static int array[1024] = {0};
+        int j = 0;
+
+        if (tree->left != NULL)
+             j += 1;
+        if (tree->right != NULL)
+             j += 2;
+        return (j);
+}
+/**
+ * binary_tree_is_complete - a function that checks
+ * if a binary tree is complete
+ *
+ * @tree: a pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is a complete
+ * binary tree or 0 otherwise
+ *
+ * If tree is NULL, your function must return 0
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+        static int count = 1, check = 1, check1 = 1, flag = 1, z = 1, k = 1;
+        int j = 0, i = 1;
 
         if (tree == NULL)
-		return (NULL);
-        if (i == count)
+             return (0);
+        j = sides_check(tree);
+        if ((check1 == 0 && count <= k - 2 && j != 3) ||
+                ((check == 0 && count >= z && j != 0) || (j == 2)) ||
+                (check1 == 0 && check == 0 && k != z + 1) ||
+                (check1 == 0 && count > k))
+             flag = 0;
+        if (flag == 0 && count == 1)
         {
-		array[idx - 1] = tree->n;
-		idx++;
-                flag = 2;
+             flag = 1;
+             check = 1;
+             check1 = 1;
+             z = 1;
+             k = 1;
+             return (0);
         }
-	if (count == i - 1)
-	{
-		if (tree->left == NULL)
-		{
-			array[idx - 1] = -99;
-			idx++;
-		}
-		   if (tree->right == NULL)
-                {
-                        array[idx - 1] = -99;
-                        idx++;
-                }
-	}
+        if (flag == 0)
+             return (0);
+        if (j == 1 && check == 1)
+        {
+             check = 0;
+             z = count;
+        }
+        if (j == 0 && check1 == 1)
+        {
+             k = count;
+             check1 = 0;
+        }
         if (tree->left != NULL)
         {
-                count++;
-		is_heap_helper(tree->left);
-                count--;
-        }	
+             count++;
+             binary_tree_is_complete(tree->left);
+             count--;
+        }
         if (tree->right != NULL)
         {
-                count++;
-		is_heap_helper(tree->right);
-                count--;
+             count++;
+             binary_tree_is_complete(tree->right);
+             count--;
         }
         if (count == 1)
         {
-                if (flag == 1)
-		{
-			i = 1;
-			array[idx - 7] = 0;
-			idx = 1;
-			return (array);
-		}
-                i++;
-                flag = 1;
-		is_heap_helper(tree);
+             i = flag;
+             flag = 1;
+             check = 1;
+             check1 = 1;
+             z = 1;
+             k = 1;
         }
-	return array;
+        return (i);
 }
 int is_max_helper(const binary_tree_t *tree)
 {
@@ -94,24 +123,11 @@ int is_max_helper(const binary_tree_t *tree)
 }
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	int *array = NULL;
-	int i = 0, flag = 0;
-
 	if (tree == NULL)
 		return (0);
 	
 	if (is_max_helper(tree) == 0)
 		return (0);
 	
-	array = is_heap_helper(tree);
-
-	while (array[i] != 0)
-	{
-		if (flag == 1 && array[i] != -99)
-			return (0);
-		if (array[i] == -99)
-			flag = 1;
-		i++;
-	}
-	return (1);
+	return (binary_tree_is_complete(tree));
 }
